c6ed8fdb375d493878f73db4795f2d7dfa2f9bf3
VQ <vqueis@gmail.com>
2022-12-07 14:18:37 +0000

    updated ex 2 docker

--- exercises/verena_ex/docker_ex_2.py
+++ exercises/verena_ex/docker_ex_2.py
@@ -1,12 +1,16 @@
-docker image pull devopsdockeruh/simple-web-service:ubuntu
+docker image pull devopsdockeruh/simple-web-service:ubuntu 
 docker ps
 docker exec -it musing_swanson /bin/bash
 tail -f ./text.log
 control c
 exit
 
-or
+#or
 
 docker pull devopsdockeruh/simple-web-service:ubuntu
 docker ps
 exec -it musing_swanson /bin/bash
+
+#to stop the docker from running
+#first type docker ps to see if it is running, then write
+docker stop musing_swanson
