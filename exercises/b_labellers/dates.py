7e6819aab3e1dffba24d71a1b66676f39b4029dc
VQ <vqueis@gmail.com>
2022-12-07 09:41:39 +0000

    exercises updated extra credit

--- exercises/b_labellers/dates.py
+++ exercises/b_labellers/dates.py
@@ -1,15 +1,21 @@
 import datetime
-
+import holidays
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import dayofweek
+from pyspark.sql.functions import dayofyear
+from pyspark.sql.functions import udf
 
 
 def is_belgian_holiday(date: datetime.date) -> bool:
+    be_holidays = holidays.country_holidays('BE')
+    return date in be_holidays
     pass
 
 
 def label_weekend(
     frame: DataFrame, colname: str = "date", new_colname: str = "is_weekend"
 ) -> DataFrame:
+    return frame.withColumn(new_colname, dayofweek(colname).isin(1,7))
     """Adds a column indicating whether or not the attribute `colname`
     in the corresponding row is a weekend day."""
     pass
@@ -20,6 +26,8 @@ def label_holidays(
     colname: str = "date",
     new_colname: str = "is_belgian_holiday",
 ) -> DataFrame:
+    extract_be_holidays = udf(is_belgian_holiday(colname), BooleanType())
+    return frame.withColumn(new_colname, dayofyear(colname).extract_be_holidays)
     """Adds a column indicating whether or not the column `colname`
     is a holiday."""
     pass
